61f7e9e375ffadbba45e1893ce45e4785d1e74fc
soldesk <soldesk@DESKTOP-10540Q7>
2024-08-07 10:15:13 +0900

    수정1

---
+++ TestSpring/src/main/java/kr/co/softsoldesk/config/RootAppContext.java
@@ -0,0 +1,57 @@
+package kr.co.softsoldesk.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Configuration//스프링 컨테이너
+@ComponentScan("kr.co.softsoldesk.service")
+@ComponentScan("kr.co.softsoldesk.DAO")
+@PropertySource("/WEB-INF/properties/db.properties")
+public class RootAppContext {
+//프로젝트 작업 시 사용할 bean을 관리하는 클래스
+	
+	@Value("${db.classname}")
+	private String db_classname;
+	
+	@Value("${db.url}")
+	private String db_url;
+	
+	@Value("${db.username}")
+	private String db_username;
+	
+	@Value("${db.password}")
+	private String db_password;
+
+	
+	//데이터베이스 접속정보 관리 Bean
+	@Bean
+	public BasicDataSource dataSource() {
+		
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName(db_classname);
+		source.setUrl(db_url);
+		source.setUsername(db_username);
+		source.setPassword(db_password);
+		
+		return source;
+		
+	}
+	//쿼리문과 접속정보를 관리 Bean
+	@Bean
+	public SqlSessionFactory factory(BasicDataSource souce) throws Exception {
+		
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(souce);
+		SqlSessionFactory factory = factoryBean.getObject();
+		
+		return factory;
+	}
+	
+}
---
+++ TestSpring/src/main/java/kr/co/softsoldesk/controller/MainController.java
@@ -0,0 +1,20 @@
+package kr.co.softsoldesk.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+	@GetMapping("/main")
+	private String main(Model model) {
+
+		return "main";
+	}
+}
